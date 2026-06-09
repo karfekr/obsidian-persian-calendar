c7f6215c1decd8d1c26ececf8b30b90b57a52d10
NedaMani <alireza3205@gmail.com>
2026-06-09 14:20:53 +0330

    chore: docs

--- src/constants/releaseNotes/index.ts
+++ src/constants/releaseNotes/index.ts
@@ -3,6 +3,13 @@ import type { TReleaseNote } from "src/types";
 //! ترتیب مهمه - جدیدترین ورژن ابتدا قرار میگیره
 export const RELEASE_NOTES: TReleaseNote[] = [
 	{
+		version: "4.5.11",
+		changes: {
+			fa: ["بهبود پلاگین طبق پیشنهادات جدید Obsidian Scorecard"],
+			en: ["Code improvements based on Obsidian Scorecard suggestions"],
+		},
+	},
+	{
 		version: "4.5.10",
 		changes: {
 			fa: ["بهبود پلاگین طبق پیشنهادات جدید Obsidian Scorecard"],
