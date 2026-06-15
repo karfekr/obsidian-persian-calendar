95dbae46d810ca04ff11067c7b7edb7f4f309b9a
Alireza Mohammadzadeh (NedaMani) <alireza3205@gmail.com>
2026-06-15 20:14:19 +0330

    feat: release-notes scripts

--- scripts/get-release-notes.ts
+++ scripts/get-release-notes.ts
@@ -1,15 +1,17 @@
-import { RELEASE_NOTES } from "../src/constants/releaseNotes/index.ts";
-import fs from "fs";
+import { RELEASE_NOTES } from "../src/constants/releaseNotes/index";
+import { writeFileSync } from "fs";
 
 const version = process.env.GITHUB_REF_NAME;
+if (!version) {
+	console.error("No GITHUB_REF_NAME found");
+	process.exit(1);
+}
 
 const found = RELEASE_NOTES.find((n) => n.version === version);
-
 if (!found) {
 	console.error("No release notes found for version", version);
 	process.exit(1);
 }
 
 const body = found.changes.en.map((c) => `- ${c}`).join("\n");
-
-fs.writeFileSync("release_body.txt", body);
+writeFileSync("release_body.txt", body, "utf-8");
